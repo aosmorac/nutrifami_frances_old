445132fb75f23522799aabbe4b2e3235ab77adba
Alejandro Tunaroza <alejandro.tunaroza@gmail.com>
2016-06-25 13:08:09 -0500

    Ver. Alpha.0.0.2 (24/06/2016) - [FIX] Login de usuario con token. - [FIX] Organización del esquema del proyecto.

--- public/js/mainController.js
+++ public/js/mainController.js
@@ -1,21 +1,16 @@
 /*
  * Configuración de angular para la aplicación Web de nutrifami
  */
-
+dependencies = ['Authentication', 'ngRoute', 'ngCookies'];
 'use strict';
 
 // declare modules
 angular.module('Authentication', []);
-angular.module('Home', []);
 
-angular.module('nutrifamiWeb', [
-    'Authentication',
-    'Home',
-    'ngRoute',
-    'ngCookies'
-])
+angular.module('NutrifamiWeb', dependencies)
 
-.config(['$routeProvider', function ($routeProvider) {
+.config(['$routeProvider', function($routeProvider) {
+    'use strict';
 
     $routeProvider
         .when('/login', {
@@ -23,29 +18,33 @@ angular.module('nutrifamiWeb', [
             templateUrl: 'modules/authentication/views/login.html',
             hideMenus: true
         })
- 
-        .when('/', {
-            controller: 'HomeController',
-            templateUrl: 'modules/home/views/home.html'
-        })
- 
-        .otherwise({ redirectTo: '/login' });
+
+    .when('/', {
+        controller: 'HomeController',
+        templateUrl: 'views/home.tpl.html'
+    })
+
+    .otherwise({ redirectTo: '/login' });
 }])
 
- 
+
 .run(['$rootScope', '$location', '$cookieStore',
-    function ($rootScope, $location, $cookieStore) {
+    function($rootScope, $location, $cookieStore) {
         // keep user logged in after page refresh
         $rootScope.globals = $cookieStore.get('globals') || {};
         nutrifami.getSessionId();
+        console.log($rootScope.globals.currentUser);
+
         if ($rootScope.globals.currentUser) {
-            $http.defaults.headers.common['Authorization'] = 'Basic ' + $rootScope.globals.currentUser.authdata; // jshint ignore:line
+            //$http.defaults.headers.common['Authorization'] = 'Basic ' + $rootScope.globals.currentUser.authdata; // jshint ignore:line
+
         }
- 
-        $rootScope.$on('$locationChangeStart', function (event, next, current) {
+
+        $rootScope.$on('$locationChangeStart', function(event, next, current) {
             // redirect to login page if not logged in
             if ($location.path() !== '/login' && !$rootScope.globals.currentUser) {
                 $location.path('/login');
             }
         });
-    }]);
+    }
+]);
--- public/modules/authentication/controllers.js
+++ public/modules/authentication/controllers.js
@@ -1,18 +1,18 @@
-'use strict';
- 
 angular.module('Authentication')
- 
-.controller('LoginController',
-    ['$scope', '$rootScope', '$location', 'AuthenticationService',
-    function ($scope, $rootScope, $location, AuthenticationService) {
+
+.controller('LoginController', ['$scope', '$rootScope', '$location', 'AuthenticationService',
+    function($scope, $rootScope, $location, AuthenticationService) {
+        'use strict';
         // reset login status
         AuthenticationService.ClearCredentials();
- 
-        $scope.login = function () {
+
+        $scope.login = function() {
             $scope.dataLoading = true;
             AuthenticationService.Login($scope.username, $scope.password, function(response) {
-                if(response.success) {
-                    AuthenticationService.SetCredentials($scope.username, $scope.password);
+                if (response.success) {
+
+                    AuthenticationService.SetCredentials($scope.username, $scope.password, response.message);
+
                     $location.path('/');
                 } else {
                     $scope.error = response.message;
