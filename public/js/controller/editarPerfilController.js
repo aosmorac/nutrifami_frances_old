f917434891f36eeff554dcd4f9d1b1a04e8aeb74
Alejandro Tunaroza <alejandro.tunaroza@gmail.com>
2016-06-28 22:39:30 -0500

    Formulario editar perfil con validación

--- public/js/controller/editarPerfilController.js
+++ public/js/controller/editarPerfilController.js
@@ -1,6 +1,52 @@
 /*global angular*/
-angular.module('NutrifamiWeb')
-    .controller('EditarPerfilController', ['$rootScope', '$scope', '$location', function($rootScope, $scope, $location) {
+angular.module('NutrifamiWeb').controller('EditarPerfilController', ['$scope', function ($scope) {
         'use strict';
-        console.log("Editar perfil");
-    }]);
+
+        var usuarioA = JSON.parse(localStorage.getItem('usuarioActivo'));
+
+        if (usuarioA.genero === 'F') {
+            $scope.genero = 'femenino';
+        } else {
+            $scope.genero = 'masculino';
+        }
+        $scope.usuario = {};
+        $scope.usuario.nombre = usuarioA.nombre;
+        $scope.usuario.apellido = usuarioA.apellido;
+        $scope.usuario.documento = usuarioA.login_documento;
+        $scope.usuario.generos = {
+            availableOptions: [
+                {id: 'F', name: 'Femenino'},
+                {id: 'M', name: 'Masculino'},
+            ],
+            selectedOption: {id: usuarioA.genero, name: $scope.genero}
+        };
+        $scope.usuario.etnias = {
+            availableOptions: [
+                {id: 'AFROCOLOMBIANOS', name: 'Afrocolombianos'},
+                {id: 'INDIGENA', name: 'Indigenas'},
+                {id: 'MESTIZO', name: 'Mestizo'},
+                {id: 'OTROS', name: 'Otros'},
+                {id: 'NINGUNO', name: 'Ninguno'}
+            ],
+            selectedOption: {id: usuarioA.etnia, name: usuarioA.etnia} //This sets the default value of the select in the ui
+        };
+
+        $scope.usuario.nacimiento = new Date(); //Verificar por el valor real de la base de datos.
+
+        /* Falta incluir en los valores de departamento y municipio de la base de datos*/
+
+        $scope.usuario.comunidad = usuarioA.comunidad;
+
+        $scope.update = function () {
+            console.log($scope.usuario);
+            console.log("Enviar a la API para actualizar la base de datos");
+        };
+    }]).filter('capitalize', function () {
+    'use strict';
+    return function (input, all) {
+        var reg = (all) ? /([^\W_]+[^\s-]*) */g : /([^\W_]+[^\s-]*)/;
+        return (!!input) ? input.replace(reg, function (txt) {
+            return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
+        }) : '';
+    };
+});
--- public/js/mainController.js
+++ public/js/mainController.js
@@ -42,8 +42,7 @@ angular.module('NutrifamiWeb', dependencies)
         // keep user logged in after page refresh
         $rootScope.globals = $cookieStore.get('globals') || {};
         nutrifami.getSessionId();
-        console.log($rootScope.globals.currentUser);
-
+        
         if ($rootScope.globals.currentUser) {
             //$http.defaults.headers.common['Authorization'] = 'Basic ' + $rootScope.globals.currentUser.authdata; // jshint ignore:line
 
