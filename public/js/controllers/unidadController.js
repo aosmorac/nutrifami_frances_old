eebf800828cce716e5f3d9a46322350188f77b40
Alejandro Tunaroza <alejandro.tunaroza@gmail.com>
2016-07-12 10:00:10 -0500

    Actividad parejas primera versión. Correción de estilos

--- public/js/controllers/unidadController.js
+++ public/js/controllers/unidadController.js
@@ -7,6 +7,7 @@ angular.module('NutrifamiWeb')
                 $scope.estadoUnidad = 'espera';
                 $scope.uids = {};
                 $scope.unidades = [];
+                $scope.parejas = [];
 
                 var tempUnidad = JSON.parse(localStorage.getItem('unidad'));
                 var cargarUnidad = true;
@@ -20,32 +21,38 @@ angular.module('NutrifamiWeb')
 
                     try {
 
-                        $scope.uids = nutrifami.training.getUnidadesId($routeParams.leccion);
-                        var temp = [];
-                        for (var i in $scope.uids) {
-                            temp.push($scope.uids[i]);
-                        }
-                        $scope.unidad = nutrifami.training.getUnidad(temp[$routeParams.unidad - 1]);
-                        $scope.unidad.avanceLeccion = (100 / temp.length) * (parseInt($routeParams.unidad) - 1);
-                        $scope.unidad.totalUnidades = temp.length;
-                        var tempOpciones = [];
-                        for (var i in $scope.unidad.opciones) {
-                            tempOpciones.push($scope.unidad.opciones[i]);
-                        }
-                        $scope.unidad.opciones = tempOpciones;
+                    $scope.uids = nutrifami.training.getUnidadesId($routeParams.leccion);
+                    var temp = [];
+                    for (var i in $scope.uids) {
+                        temp.push($scope.uids[i]);
+                    }
+                    $scope.unidad = nutrifami.training.getUnidad(temp[$routeParams.unidad - 1]);
+                    $scope.unidad.avanceLeccion = (100 / temp.length) * (parseInt($routeParams.unidad) - 1);
+                    $scope.unidad.totalUnidades = temp.length;
+                    var tempOpciones = [];
+                    for (var i in $scope.unidad.opciones) {
+                        tempOpciones.push($scope.unidad.opciones[i]);
+                    }
+                    $scope.unidad.opciones = tempOpciones;
 
+
+                    if (typeof $scope.unidad.imagen !== 'undefined') {
+                        console.log("imagen no definida");
                         if ($scope.unidad.imagen.loaded === "loaded") {
                             $scope.unidad.imagen.mostrar = true;
                         } else {
                             $scope.unidad.imagen.mostrar = false;
                         }
-
-                        localStorage.setItem("uids" + $routeParams.leccion, JSON.stringify(temp));
-                        localStorage.setItem("unidad", JSON.stringify($scope.unidad));
-                    } catch (err) {
-                        alert("Error");
-                        $location.path('/');
                     }
+
+                    console.log($scope.unidad);
+
+                    localStorage.setItem("uids" + $routeParams.leccion, JSON.stringify(temp));
+                    localStorage.setItem("unidad", JSON.stringify($scope.unidad));
+                     } catch (err) {
+                     alert("Error");
+                     $location.path('/');
+                     }
                 } else {
                     $scope.uids = JSON.parse(localStorage.getItem('uids' + $routeParams.leccion));
                     $scope.unidad = JSON.parse(localStorage.getItem('unidad'));
@@ -54,19 +61,130 @@ angular.module('NutrifamiWeb')
                 if ($scope.unidad.opciones.length == 3 || $scope.unidad.opciones.length == 6) {
                     $scope.unidad.colspan = 4;
                 } else if ($scope.unidad.opciones.length == 2) {
-                    $scope.unidad.opciones.colspan == 6;
+                    $scope.unidad.opciones.colspan = 6;
                 } else {
                     $scope.unidad.colspan = 3;
                 }
 
+                /*  DATOS DUMMY PARA PROBAR PAREJAS */
+                /*var opciones = [
+                 {
+                 id: 9,
+                 correcta: 0,
+                 orden: 1,
+                 fecha: '',
+                 visible: true,
+                 texto: 'Texto uno',
+                 audio: {
+                 nombre: '',
+                 content: Object,
+                 loaded: false
+                 },
+                 media: {
+                 nombre: '',
+                 content: Object,
+                 loaded: false
+                 }
+                 }, {
+                 id: 9,
+                 correcta: 0,
+                 orden: 2,
+                 fecha: '',
+                 visible: true,
+                 texto: 'Texto dos',
+                 audio: {
+                 nombre: '',
+                 content: Object,
+                 loaded: false
+                 },
+                 media: {
+                 nombre: '',
+                 content: Object,
+                 loaded: false
+                 }
+                 }, {
+                 id: 9,
+                 correcta: 0,
+                 orden: 2,
+                 fecha: '',
+                 visible: true,
+                 texto: 'Texto dos',
+                 audio: {
+                 nombre: '',
+                 content: Object,
+                 loaded: false
+                 },
+                 media: {
+                 nombre: '',
+                 content: Object,
+                 loaded: false
+                 }
+                 }, {
+                 id: 9,
+                 correcta: 0,
+                 orden: 1,
+                 fecha: '',
+                 visible: true,
+                 texto: 'Texto uno',
+                 audio: {
+                 nombre: '',
+                 content: Object,
+                 loaded: false
+                 },
+                 media: {
+                 nombre: '',
+                 content: Object,
+                 loaded: false
+                 }
+                 }, {
+                 id: 9,
+                 correcta: 0,
+                 orden: 3,
+                 fecha: '',
+                 visible: true,
+                 texto: 'Texto tres',
+                 audio: {
+                 nombre: '',
+                 content: Object,
+                 loaded: false
+                 },
+                 media: {
+                 nombre: '',
+                 content: Object,
+                 loaded: false
+                 }
+                 }, {
+                 id: 9,
+                 correcta: 0,
+                 orden: 3,
+                 fecha: '',
+                 visible: true,
+                 texto: 'Texto tres',
+                 audio: {
+                 nombre: '',
+                 content: Object,
+                 loaded: false
+                 },
+                 media: {
+                 nombre: '',
+                 content: Object,
+                 loaded: false
+                 }
+                 }];
+                 
+                 $scope.unidad.opciones = opciones;*/
+                /*  FIN - DATOS DUMMY PARA PROBAR PAREJAS */
+
+
                 /*
                  * Se define el tipo de pregunta para adaptar el funcionamiento
-                 * id = 1; Opción multiple con única respuesta, y verdadero o falso.
-                 * id = 2; Opción multiple con multiple respuesta
-                 * id = 3; Parejas
+                 * id = 1 ; Contenidos
+                 * id = 2; Parejas
+                 * id = 3,4,5; Opción multiple con única respuesta, y verdadero o falso.
                  * 
                  */
-                var tipoPregunta = $scope.unidad.tipo.id;
+                $scope.tipoPregunta = $scope.unidad.tipo.id;
+                /* $scope.tipoPregunta = 2;*/
 
                 /* Obtenemos la cantidad de preguntas correctas*/
                 var respuestasCorrectas = 0;
@@ -82,6 +200,8 @@ angular.module('NutrifamiWeb')
                     $scope.unidad.opciones[i].selected = false;
                     $scope.unidad.opciones[i].evaluacion = false;
                     $scope.unidad.opciones[i].sticker = 'remove mal';
+                    $scope.unidad.opciones[i].pareja = '';
+                    $scope.unidad.opciones[i].match = '';
                 }
 
 
@@ -109,6 +229,7 @@ angular.module('NutrifamiWeb')
                                 if ($scope.unidad.opciones[i].selected) {
                                     $scope.unidad.opciones[i].estilo = '';
                                     $scope.unidad.opciones[i].selected = false;
+                                    $scope.unidad.opciones[i].evaluacion = false;
                                     respuestasSeleccionadas--;
                                 }
                             }
@@ -122,6 +243,83 @@ angular.module('NutrifamiWeb')
                         $scope.botonCalificar.estilo = 'no-activo';
                         $scope.botonCalificar.disabled = 'disabled';
                     }
+
+                };
+
+                var parejasContador = 0;
+                var pareja1Orden = 0;
+                var pareja2Orden = 0;
+                var pareja1Pos = 0;
+                var pareja2Pos = 0;
+                var parejasCorrectas = 0;
+
+                $scope.seleccionarPareja = function (index) {
+                    if ($scope.unidad.opciones[index].selected) {
+                        $scope.unidad.opciones[index].pareja = '';
+                        $scope.unidad.opciones[index].selected = false;
+
+                        /*Borrar la respuesta correcta para validar más adelante si es una pareja*/
+                        //respuestasSeleccionadas--;
+                        parejasContador--;
+
+
+                    } else {
+                        $scope.unidad.opciones[index].pareja = 'pareja-' + $scope.unidad.opciones[index].orden;
+                        $scope.unidad.opciones[index].selected = true;
+                        /*Almacenar la respuesta correcta para validar más adelante si es una pareja*/
+                        //respuestasSeleccionadas++;
+                        parejasContador++;
+                        if (parejasContador == 1) {
+                            pareja1Orden = $scope.unidad.opciones[index].orden;
+                            pareja1Pos = index;
+                        } else if (parejasContador == 2) {
+                            pareja2Orden = $scope.unidad.opciones[index].orden;
+                            pareja2Pos = index;
+
+                            if (pareja1Orden == pareja2Orden) {
+                                console.log("Hay Match");
+
+                                /*Estilos para la pareja actual*/
+                                $scope.unidad.opciones[pareja2Pos].pareja = 'pareja-' + pareja2Orden;
+                                $scope.unidad.opciones[pareja2Pos].selected = false;
+                                $scope.unidad.opciones[pareja2Pos].match = 'match';
+
+                                /*Estilos para pareja anterior*/
+                                $scope.unidad.opciones[pareja1Pos].pareja = 'pareja-' + pareja2Orden;
+                                $scope.unidad.opciones[pareja1Pos].selected = false;
+                                $scope.unidad.opciones[pareja1Pos].match = 'match';
+
+                                parejasContador = 0;
+                                pareja1Pos = 0;
+                                pareja2Pos = 0;
+                                pareja1Orden = 0;
+                                pareja2Orden = 0;
+
+                                parejasCorrectas++;
+                                console.log(parejasCorrectas + " parejas correctas de " + ($scope.unidad.opciones.length / 2));
+
+                                if (parejasCorrectas == ($scope.unidad.opciones.length / 2)) {
+                                    /*Si las parejas correctas es igual a la mitad de la cantidad de opciones habilitar el botón de continuar*/
+                                    $scope.estadoUnidad = 'acierto';
+                                }
+
+                            } else {
+                                $scope.unidad.opciones[pareja2Pos].pareja = '';
+                                $scope.unidad.opciones[pareja2Pos].selected = false;
+                                $scope.unidad.opciones[pareja2Pos].match = '';
+                                $scope.unidad.opciones[pareja1Pos].pareja = '';
+                                $scope.unidad.opciones[pareja1Pos].selected = false;
+                                $scope.unidad.opciones[pareja1Pos].match = '';
+                                parejasContador = 0;
+                                pareja1Pos = 0;
+                                pareja2Pos = 0;
+                                pareja1Orden = 0;
+                                pareja2Orden = 0;
+
+                            }
+                        }
+                    }
+
 
                 };
 
@@ -147,14 +345,11 @@ angular.module('NutrifamiWeb')
 
                 $scope.irASiguienteUnidad = function () {
                     $scope.siguienteUnidad = parseInt($routeParams.unidad) + 1;
-                    console.log(parseInt($routeParams.unidad) + 1);
-                    console.log($scope.unidad.totalUnidades);
-                    console.log($scope.siguienteUnidad > $scope.unidad.totalUnidades);
                     if ($scope.siguienteUnidad > $scope.unidad.totalUnidades) {
                         var avanceUsuario = JSON.parse(localStorage.getItem('avanceUsuario'));
                         var lids = JSON.parse(localStorage.getItem('lids' + $routeParams.modulo));
                         var indice = 0;
-                        
+
                         /*Busca el indice basado en el id de la lección para actualizar e avance*/
                         for (var i in lids) {
                             if (lids[i] == $routeParams.leccion) {
@@ -162,7 +357,7 @@ angular.module('NutrifamiWeb')
                             }
                         }
                         avanceUsuario.lecciones[indice - 1] = 1;
-                        
+
                         /* Mira cuantas lecciones se han terminaddo para dar un resultado final */
                         var contador = 0;
                         for (var i in avanceUsuario.lecciones) {
@@ -181,15 +376,12 @@ angular.module('NutrifamiWeb')
 
                 $scope.reiniciarUnidad = function () {
                     for (var i in $scope.unidad.opciones) {
-                        if ($scope.unidad.opciones[i].selected) {
-                            $scope.unidad.opciones[i].estilo = '';
-                            $scope.unidad.opciones[i].selected = false;
-                            $scope.unidad.opciones[i].evaluacion = false;
-                            $scope.unidad.opciones[i].sticker = 'remove mal';
-                            respuestasSeleccionadas = 0;
-                        }
+                        $scope.unidad.opciones[i].estilo = '';
+                        $scope.unidad.opciones[i].selected = false;
+                        $scope.unidad.opciones[i].evaluacion = false;
+                        $scope.unidad.opciones[i].sticker = 'remove mal';
+                        respuestasSeleccionadas = 0;
                     }
-
                     $scope.estadoUnidad = 'espera';
                     $scope.botonCalificar.estilo = 'no-activo';
                     $scope.botonCalificar.disabled = 'disabled';
@@ -207,6 +399,22 @@ angular.module('NutrifamiWeb')
                 link: function ($scope, $element, $attrs) {
                     $scope.click = function () {
                         $scope.$parent.seleccionarOpcion($scope.index);
+                    };
+                }
+
+            };
+        })
+        .directive('parejasUnidadInfo', function () {
+            return {
+                restrict: 'E',
+                scope: {
+                    info: '=',
+                    index: '@'
+                },
+                templateUrl: 'views/directives/parejasUnidadInfo.html',
+                link: function ($scope, $element, $attrs) {
+                    $scope.click = function () {
+                        $scope.$parent.seleccionarPareja($scope.index);
                     };
                 }
 
